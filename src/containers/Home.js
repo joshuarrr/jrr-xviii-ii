cde677d4bcc42f72edef6913ee787e3f212d7981
Joshua Richey <j@joshuar.com>
2018-07-06 20:54:13 -0700

    sample form

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,5 +7,19 @@ export default withSiteData(() => (
   <div>
     <h1 style={{ textAlign: 'center' }}>Hello World</h1>
     <img src={logoImg} alt="" />
+    <form name="contact" method="POST" netlify>
+      <p>
+        <label htmlFor="name">Your Name: <input id="text" type="name" name="name" /></label>
+      </p>
+      <p>
+        <label htmlFor="email">Your Email: <input id="email" type="email" name="email" /></label>
+      </p>
+      <p>
+        <label htmlFor="message">Message: <textarea id="message" name="message" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </div>
 ))
