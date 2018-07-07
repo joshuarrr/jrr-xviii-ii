7239c9c43682da18ee7c735deb59b4da4745a06d
Joshua Richey <j@joshuar.com>
2018-07-06 21:10:49 -0700

    netlify form testing

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,7 +7,8 @@ export default withSiteData(() => (
   <div>
     <h1 style={{ textAlign: 'center' }}>Hello World</h1>
     <img src={logoImg} alt="" />
-    <form name="contact" method="POST" netlify>
+    <form name="contact" method="POST" netlify netlify-honeypot="bot-field" hidden>
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label htmlFor="name">Your Name: <input id="text" type="name" name="name" /></label>
       </p>
