6cad6d7568e25e542833a9343abb60e9e24439d0
Joshua Richey <j@joshuar.com>
2018-07-08 16:13:52 -0700

    default animated routes config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,113 @@
 import React from 'react'
-import { Router, Link } from 'react-static'
+import { Router, Route, Link, cleanPath } from 'react-static'
 import { hot } from 'react-hot-loader'
 import Routes from 'react-static-routes'
+import { NodeGroup } from 'react-move'
+import { withContext, getContext } from 'recompose'
+import PropTypes from 'prop-types'
 import { Logo } from './components/logo'
 
 import './styles/app.css'
+
+// The magic :)
+const AnimatedRoutes = getContext({
+  // We have to preserve the router context for each route
+  // otherwise, a component may rerender with the wrong data
+  // during animation
+  router: PropTypes.object,
+  // We'll also look for the staticURL, so we can disable the animation during static render
+  staticURL: PropTypes.string,
+})(({ getComponentForPath, router, staticURL }) => (
+  <Route
+    path="*"
+    render={props => {
+      // Get the component for this path
+      let Comp = getComponentForPath(cleanPath(props.location.pathname))
+      if (!Comp) {
+        Comp = getComponentForPath('404')
+      }
+
+      // When we're rendering for static HTML, be sure to NOT animate in.
+      if (staticURL) {
+        return (
+          // This relative wrapper is necessary for accurate rehydration :)
+          <div style={{ position: 'relative' }}>
+            <Comp {...props} />
+          </div>
+        )
+      }
+
+      // Use React-Move to animate the different components coming in and out
+      return (
+        <NodeGroup
+          // React-move will handle the entry and exit of any items we pass in `data`
+          data={[
+            {
+              // pass the current Comp, props, ID and router
+              id: props.location.pathname,
+              Comp,
+              props,
+              router,
+            },
+          ]}
+          keyAccessor={d => d.id}
+          start={() => ({
+            opacity: [0],
+            scale: 1,
+            translateY: [10],
+          })}
+          enter={() => ({
+            opacity: [1],
+            translateY: [0],
+            timing: { duration: 200, delay: 200 },
+          })}
+          update={() => ({
+            opacity: [1],
+          })}
+          leave={() => ({
+            opacity: [0],
+            translateY: [-10],
+            timing: { duration: 200 },
+          })}
+        >
+          {nodes => (
+            <div style={{ position: 'relative' }}>
+              {nodes.map(({ key, data, state: { opacity, translateY } }) => {
+                // Here, we override the router context with the one that was
+                // passed with each route
+                const PreservedRouterContext = withContext(
+                  {
+                    router: PropTypes.object,
+                  },
+                  () => ({
+                    router: data.router,
+                  }),
+                )(props => <div {...props} />)
+
+                return (
+                  <PreservedRouterContext
+                    key={key}
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      right: 0,
+                      bottom: 0,
+                      left: 0,
+                      transform: `translateY(${translateY}px)`,
+                      opacity,
+                    }}
+                  >
+                    <data.Comp {...data.props} />
+                  </PreservedRouterContext>
+                )
+              })}
+            </div>
+          )}
+        </NodeGroup>
+      )
+    }}
+  />
+))
 
 const App = () => (
   <Router>
@@ -18,7 +121,7 @@ const App = () => (
         </nav>
       </header>
       <main className="content">
-        <Routes />
+        <Routes component={AnimatedRoutes} />
       </main>
     </div>
   </Router>
