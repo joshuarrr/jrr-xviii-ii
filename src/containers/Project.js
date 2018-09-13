bc8265481a98af69c4028349bf281dfbfa28c5f0
Joshua Richey <j@joshuar.com>
2018-09-12 17:41:26 -0700

    wiw projects page

--- src/containers/Project.js
+++ src/containers/Project.js
@@ -3,17 +3,16 @@ import { withRouteData, Link } from 'react-static'
 import Moment from 'react-moment'
 import Markdown from 'react-markdown'
 import { Img } from '../components/image-loader'
+import '../styles/projects.css'
 
 
 export default withRouteData(({ project }) => (
   <div className="page-content project-page">
     <div className="project">
-      <Link to="/projects/">{'<'} Back</Link>
-      <br />
-      <h3>{project.data.title}</h3>
-      <Moment format="MMMM Do, YYYY">{project.data.date}</Moment>
-      <Img name={project.data.slug} />
-      <Markdown source={project.content} escapeHtml={false} />
+      {/*<Link to="/projects/">{'<'} Back</Link>*/}
+      <h2 className="project-title">{project.data.title}</h2>
+      <Img className="project-cover" name={project.data.slug} />
+      <Markdown className="project-content" source={project.content} escapeHtml={false} />
     </div>
   </div>
 ))
