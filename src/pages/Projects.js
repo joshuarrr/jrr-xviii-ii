94b133df12d62848aca11ac283c611239fd9aae5
Eric O'Connell <eric@roundpegdesigns.com>
2018-09-14 15:49:52 -0700

    I think this would work?

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,20 +12,11 @@ export class CurrentProject extends Component {
     }
   }
   render () {
-    return(
-      <p>{this.state.currentProject}</p>
-    )
-  }
-}
-
-export default withRouteData(({ projects }) => {
-  
-  {console.log(projects);}
-  return (
-    <div className="page-content projects-page">
-      <ul>
-        {projects.map(project => (
-          <li className="project" key={project.data.slug}>
+    return (
+      <div className="page-content projects-page">
+        <ul>
+          {this.props.projects.map(project => (
+            <li className="project" key={project.data.slug}>
               <Link to={`projects/project/${project.data.slug}`}>
                 <h2 className="project-title">
                   {project.data.title}
@@ -35,23 +26,26 @@ export default withRouteData(({ projects }) => {
                   name={"portfolio/" + project.data.slug}
                 />
               </Link>
-            <Markdown
-              className="project-content"
-              source={project.data.summary}
-              escapeHtml={false}
-            />
-            <Link className="project-more-link" to={`projects/project/${project.data.slug}`}>
-              view project
-            </Link>
+              <Markdown
+                className="project-content"
+                source={project.data.summary}
+                escapeHtml={false}
+              />
+              <Link className="project-more-link" to={`projects/project/${project.data.slug}`}>
+                view project
+              </Link>
+            </li>
+          ))}
+          <li className="project" key="past work">
+            Previous Projects
           </li>
-        ))}
-        <li className="project" key="past work">
-          Previous Projects
-        </li>
-      </ul>
-      <MediaQuery minWidth={1024}>
-        <CurrentProject />
-      </MediaQuery>
-    </div>
-  )
-})
+        </ul>
+        <MediaQuery minWidth={1024}>
+          <CurrentProject />
+        </MediaQuery>
+      </div>
+    )
+  }
+}
+
+export default withRouteData(({ projects }) => <CurrentProject projects={projects} />)
