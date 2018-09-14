a12862781f3c6c112a20b7158193e268bd36c219
Joshua Richey <j@joshuar.com>
2018-09-14 15:02:14 -0700

    wip - current project on projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,36 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withRouteData, Link } from 'react-static'
 import { Img } from '../components/image-loader'
 import Markdown from 'react-markdown'
+import MediaQuery from 'react-responsive';
 
+export class CurrentProject extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentProject: 1
+    }
+  }
+  render () {
+    return(
+      <p>{this.state.currentProject}</p>
+    )
+  }
+}
 
-export default withRouteData(({ projects }) => (
-  <div className="page-content projects-page">
-    <ul>
-      {projects.map(project => (
-        <li className="project" key={project.data.slug}>
-            <Link to={`projects/project/${project.data.slug}`}>
-              <h2 className="project-title">
-                {project.data.title}
-              </h2>
-              <Img
-                className="project-cover"
-                name={"portfolio/" + project.data.slug}
-              />
+export default withRouteData(({ projects }) => {
+  
+  {console.log(projects);}
+  return (
+    <div className="page-content projects-page">
+      <ul>
+        {projects.map(project => (
+          <li className="project" key={project.data.slug}>
+              <Link to={`projects/project/${project.data.slug}`}>
+                <h2 className="project-title">
+                  {project.data.title}
+                </h2>
+                <Img
+                  className="project-cover"
+                  name={"portfolio/" + project.data.slug}
+                />
+              </Link>
+            <Markdown
+              className="project-content"
+              source={project.data.summary}
+              escapeHtml={false}
+            />
+            <Link className="project-more-link" to={`projects/project/${project.data.slug}`}>
+              view project
             </Link>
-          <Markdown
-            className="project-content"
-            source={project.data.summary}
-            escapeHtml={false}
-          />
-          <Link className="project-more-link" to={`projects/project/${project.data.slug}`}>
-            view project
-          </Link>
+          </li>
+        ))}
+        <li className="project" key="past work">
+          Previous Projects
         </li>
-      ))}
-      <li className="project" key="past work">
-        Previous Projects
-      </li>
-    </ul>
-  </div>
-))
+      </ul>
+      <MediaQuery minWidth={1024}>
+        <CurrentProject />
+      </MediaQuery>
+    </div>
+  )
+})
