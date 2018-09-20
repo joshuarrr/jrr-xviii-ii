a7978db1e7da44aa628fe88755ce6758decffbe7
Joshua Richey <j@joshuar.com>
2018-09-19 17:30:55 -0700

    wip on carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { withSiteData, Link } from 'react-static'
-import { Img } from '../components/image-loader'
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-img-carousel'
 import { Image } from 'cloudinary-react'
+import { Img } from '../components/image-loader'
 import '../styles/home.css'
-// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../styles/vendor/carousel.css'
+
+require('react-img-carousel/lib/carousel.css')
 
 
 export default withSiteData(({ projects }) => {
@@ -13,15 +14,7 @@ export default withSiteData(({ projects }) => {
     <div className="page-content home-page">
       <h1 className="site-title">Joshua Richey</h1>
       <h2 className="site-subtitle">Product Designer</h2>
-      <Carousel
-        showThumbs={false}
-        showStatus={false}
-        selectedItem={1}
-        infiniteLoop={true}
-        transitionTime={700}
-        emulateTouch={true}
-        useKeyboardArrows={true}
-      >
+      <Carousel>
         {
           projects.map((project) => (
             <Link key={project.data.slug} to={`projects/project/${project.data.slug}`}>
