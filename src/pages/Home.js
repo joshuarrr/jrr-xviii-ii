a07ffe687b514c2c68ac551d3162016a1bfe8187
Joshua Richey <j@joshuar.com>
2018-09-21 15:25:09 -0700

    wip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import { withSiteData, Link } from 'react-static'
-import Carousel from 'react-img-carousel'
-import { Image } from 'cloudinary-react'
+import Carousel from 'react-flex-carousel'
+import { easeQuadInOut } from 'd3-ease'
 import { Img } from '../components/image-loader'
 import '../styles/home.css'
-import '../styles/vendor/carousel.css'
 
-require('react-img-carousel/lib/carousel.css')
-
-
-export default withSiteData(({ projects }) => {
-  return (
-    <div className="page-content home-page">
-      <h1 className="site-title">Joshua Richey</h1>
-      <h2 className="site-subtitle">Product Designer</h2>
-      <Carousel>
-        {
-          projects.map((project) => (
-            <Link key={project.data.slug} to={`projects/project/${project.data.slug}`}>
-              <Img
-                className="cover-image"
-                name={`portfolio/${project.data.slug}`}
-              />
-            </Link>
-            )
+export default withSiteData(({ projects }) => (
+  <div className="page-content home-page">
+    <h1 className="site-title">Joshua Richey</h1>
+    <h2 className="site-subtitle">Product Designer</h2>
+    <Carousel
+      switcher
+      indicator
+      transitionDuration=".5s"
+      ease={easeQuadInOut}
+    >
+      {
+        projects.map(project => (
+          <Link key={project.data.slug} to={`projects/project/${project.data.slug}`}>
+            <Img
+              className="cover-image"
+              name={`portfolio/${project.data.slug}`}
+            />
+          </Link>
           )
-        }
-      </Carousel>
-    </div>
-  )
-})
+        )
+      }
+    </Carousel>
+  </div>
+))
